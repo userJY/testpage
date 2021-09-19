03ff356fd38fb8d97a20adb6df04f7960e7652f5
userJY <jy1486@nyu.edu>
2021-09-19 01:56:33 +0000

    Deploying to gh-pages from @ userJY/testpage@78e5b5051672646a49fa03d2bc0c05caf86327fa 🚀

--- _next/static/0cTDRatRprK15pRKCR4yZ/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/560-01ae9160edf98507d032.js","static/css/5dd3a863eec8dc33b66f.css","static/chunks/pages/index-f487af2e1ecb22b37f56.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
